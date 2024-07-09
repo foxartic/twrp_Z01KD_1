72dc3328c33cc2bace02522d5cedc532f029b870
HarjeetSingh <harjeet.singh5101@gmail.com>
2024-07-09 09:53:30 -0700

    Update twrp_Z01K.mk

--- twrp_Z01K.mk
+++ twrp_Z01K.mk
@@ -25,11 +25,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, device/asus/Z01K/device.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/twrp/config/common.mk)
+$(call inherit-product, vendor/omni/config/common.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := Z01K
-PRODUCT_NAME := twrp_Z01K
+PRODUCT_NAME := omni_Z01K
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := ASUS_Z01KD
 PRODUCT_MANUFACTURER := asus
